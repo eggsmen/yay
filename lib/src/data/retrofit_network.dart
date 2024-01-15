51e3203a4e16d36f04d4faa963d5fa0c65eb48fc
eggsmen <hgyu@kaist.ac.kr>
2024-01-16 02:48:18 +0900

    url 미세수정

--- lib/src/data/retrofit_network.dart
+++ lib/src/data/retrofit_network.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:sse/client/sse_client.dart';
+import 'package:menttang/src/data/baseUrl.dart';
 
 class DataFetcher {
   static Stream<String> fetchDataStream() {
@@ -7,7 +8,7 @@ class DataFetcher {
     final controller = StreamController<String>();
 
     try {
-      final url = Uri.parse('http://localhost:8080/api/chat-gpt/ask-stream/v1?question=기ㅁ태')
+      final url = Uri.parse(urlBase.url+'/chat-gpt/ask-stream/v1')
           .replace(queryParameters: {
         'question': '김태윤 교수님한테 정중하게 성적 정정 메일 써줘. 메일은 무조건 한국어로 써줘. 그리고 메일이 끝나면 end 를 한번에 보내줘.'
       }).toString();
